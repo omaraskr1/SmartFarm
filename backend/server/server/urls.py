2f618df4abc3c4cad952cffa0ef43fc8ff5cec1d
Omar Askr <omr20191700409@cis.asu.edu.eg>
2023-04-18 04:22:28 +0200

    commit

--- backend/server/server/urls.py
+++ backend/server/server/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import  include
 from django.contrib import admin
 from django.urls import path
 from django.views.static import serve
-from django.conf.urls import url
-
+# from django.conf.urls import url
+from django.conf import settings
 from apps.endpoints.urls import urlpatterns as endpoints_urlpatterns
 
 urlpatterns = [
@@ -11,8 +11,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path("", include("apps.endpoints.urls")),
     # path("", include("django_nextjs.urls")),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    path(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
+    path(r'^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}), 
 ]
 
 urlpatterns += endpoints_urlpatterns
