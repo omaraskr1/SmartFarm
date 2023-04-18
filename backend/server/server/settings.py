4d839ca535994755d4beed13cb47e373540fedc6
Omar Askr <omr20191700409@cis.asu.edu.eg>
2023-04-18 04:06:53 +0200

    try to upload to heroku

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -25,9 +25,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'xvj^n1w+!p2y^06)$v^epe#g92fcf$=sl^3jiv_z*8cv43)c0t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
+
 
 
 # Application definition
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
